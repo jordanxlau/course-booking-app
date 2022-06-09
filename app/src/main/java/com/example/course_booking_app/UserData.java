6154bc66b40b5fc30e292fbfee7f1331e9efde43
jordanxlau <jordanxlau@yahoo.ca>
2022-06-09 13:31:39 -0400

    Added database handler class, UserData

---
+++ app/src/main/java/com/example/course_booking_app/UserData.java
@@ -0,0 +1,32 @@
+package com.example.course_booking_app;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+//This class handles the database
+public class UserData extends SQLiteOpenHelper {
+
+    public final String TABLE_NAME = "users";
+    public final String PRIMARY_KEY = "";
+    public final String USER = "";
+
+    public UserData(Context context){
+        super(context, "products.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String create = "CREATE TABLE " + TABLE_NAME + "("
+                + PRIMARY_KEY + " INTEGER PRIMARY KEY,"
+                + USER + " TEXT "
+                + ")";
+        db.execSQL(create);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        String upgrade = "DROP TABLE IF EXISTS " + TABLE_NAME;
+        db.execSQL(upgrade);
+    }
+}
