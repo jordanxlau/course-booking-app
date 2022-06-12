a022e247f14c718830fdab90631fac3620426a3b
michaeltsoon <michaeltsoon@gmail.com>
2022-06-12 15:45:57 -0400

    update

--- app/src/main/java/com/example/course_booking_app/UserModal.java
+++ app/src/main/java/com/example/course_booking_app/UserModal.java
@@ -1,4 +1,49 @@
 package com.example.course_booking_app;
 
 public class UserModal {
+    private int ID;
+    private String username;
+    private String password;
+    private String usertype;
+
+    public UserModal(int ID, String username, String password, String usertype) {
+        this.ID = ID;
+        this.username = username;
+        this.password = password;
+        this.usertype = usertype;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsertype() {
+        return usertype;
+    }
+
+    public void setUsertype(String usertype) {
+        this.usertype = usertype;
+    }
+
+
 }
