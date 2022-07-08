4714b7bf85eae3b709e409a84fbaf5ed9e6c731f
EricVan14 <evand008@uottawa.ca>
2022-07-08 09:36:37 -0300

    all unit tests done

--- app/src/test/java/com/example/course_booking_app/CourseTests.java
+++ app/src/test/java/com/example/course_booking_app/CourseTests.java
@@ -15,6 +15,7 @@ public class CourseTests  {
     public void setUp(){
         testCourse = new Course("1234", "ITI1121B", "Software Eng","Eric");
     }
+
     @Test
     public void test_getCode() {
         assertEquals("ITI1121B", testCourse.getCode());
@@ -25,4 +26,21 @@ public class CourseTests  {
     public void test_getInstructor(){assertEquals("Eric",testCourse.getInstructor());}
     @Test
     public void test_getID(){assertEquals("1234",testCourse.getID());}
+    @Test
+    public void test_setCode() {
+        testCourse.setCode("ITI2101A");
+        assertEquals("ITI2101A", testCourse.getCode());
+    }
+    @Test
+    public void test_setName(){
+        testCourse.setName("Computer Architecture");
+        assertEquals("Computer Architecture",testCourse.getName());}
+    @Test
+    public void test_setInstructor(){
+        testCourse.setInstructor("Jordan");
+        assertEquals("Jordan",testCourse.getInstructor());}
+    @Test
+    public void test_setID(){
+        testCourse.setID("5678");
+        assertEquals("5678",testCourse.getID());}
 }
--- app/src/test/java/com/example/course_booking_app/UserTests.java
+++ app/src/test/java/com/example/course_booking_app/UserTests.java
@@ -17,6 +17,8 @@ public class UserTests  {
     public void setUp(){
         testUser = new User("1234", "Jordan Lau", "password123","instructor");
     }
+
+
     @Test
     public void test_getUsertype() {
         assertEquals("instructor", testUser.getUsertype());
@@ -33,7 +35,23 @@ public class UserTests  {
     public void test_getPassword(){
         assertEquals("password123",testUser.getPassword());
     }
-
+    @Test
+    public void test_setUsername() {
+        testUser.setUsername("Eric Van de Lande");
+        assertEquals("Eric Van de Lande", testUser.getUsername());
+    }
+    @Test
+    public void test_setUserType(){
+        testUser.setUsertype("student");
+        assertEquals("student",testUser.getUsertype());}
+    @Test
+    public void test_setInstructor(){
+        testUser.setPassword("password567");
+        assertEquals("password567",testUser.getPassword());}
+    @Test
+    public void test_setID(){
+        testUser.setID("5678");
+        assertEquals("5678",testUser.getID());}
 
 
 }
