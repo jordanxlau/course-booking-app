9427e289ef139f7a42eaa1ad13c021fca2c93544
jordanxlau <jordanxlau@yahoo.ca>
2022-07-31 14:00:17 -0400

    database version issue

--- app/src/main/java/com/example/course_booking_app/DatabaseHandler.java
+++ app/src/main/java/com/example/course_booking_app/DatabaseHandler.java
@@ -32,7 +32,7 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     public static final String COURSE_COL_STUDENTLIST = "courseStudentList";
 
     public DatabaseHandler(Context context){
-        super(context, "users4.db", null, 8);
+        super(context, "users4.db", null, 9);
     }
 
     public DatabaseHandler(Context context, String name, int version){
