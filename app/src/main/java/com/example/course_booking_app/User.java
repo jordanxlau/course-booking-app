4d5b20d22b0555a8a7db3b5ea0ede747e64ac1c5
jordanxlau <jordanxlau@yahoo.ca>
2022-07-30 20:27:58 -0400

    removed unecessary methods

--- app/src/main/java/com/example/course_booking_app/User.java
+++ app/src/main/java/com/example/course_booking_app/User.java
@@ -1,22 +1,16 @@
 package com.example.course_booking_app;
 
-import java.util.ArrayList;
-
 public class User {
     private String ID;
     private String username;
     private String password;
     private String usertype;
-    private ArrayList<String> availableBlocks = new ArrayList<String>(); //Represents availability of all 10 possible course blocks in a standard timetable
-    //Note: blocks start at 0
 
     public User(String ID, String username, String password, String usertype) {
         this.ID = ID;
         this.username = username;
         this.password = password;
         this.usertype = usertype;
-        for (int i = 0; i < 10; i++)
-            availableBlocks.add("true");
     }
 
     public String getID() {
@@ -25,20 +19,6 @@ public class User {
 
     public void setID(String ID) {
         this.ID = ID;
-    }
-
-    public boolean isAvailableAtBlock(int block){
-        return Boolean.parseBoolean(  availableBlocks.get(block)  );
-    }
-
-    public void addCourseAtBlock(int block){
-        if (block >= 0 && block < 10)
-            availableBlocks.set(block, "false");
-    }
-
-    public void removeCourseAtBlock(int block){
-        if (block >= 0 && block < 10)
-            availableBlocks.set(block, "true");
     }
 
     public String toString(){
