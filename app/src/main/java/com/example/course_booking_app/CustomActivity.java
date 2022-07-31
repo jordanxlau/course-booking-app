938337b50628e62886a260c1c3e665b954fd8122
jordanxlau <jordanxlau@yahoo.ca>
2022-07-30 22:17:51 -0400

    updated custom activity

--- app/src/main/java/com/example/course_booking_app/CustomActivity.java
+++ app/src/main/java/com/example/course_booking_app/CustomActivity.java
@@ -58,6 +58,13 @@ public abstract class CustomActivity extends AppCompatActivity {
         ft.commit();
     }
 
+    //Opens join course fragment
+    protected void joinCourseFragment(){
+        JoinCourseFragment fragment = new JoinCourseFragment();
+        getSupportFragmentManager().beginTransaction().setCustomAnimations(R.anim.enter_left_to_right, R.anim.exit_right_to_left).add(R.id.coordinatorLayout, fragment).commit();
+
+    }
+
     //Opens main page
     protected void openMain(){
         Intent intent = new Intent(this, MainActivity.class);
